8ffba8f40f727d452f106396dfdc6166becb6f29
alex <aeg0204@gmail.com>
2017-05-27 20:42:12 -0600

    Add initial messaging view

---
+++ app/src/main/java/github/com/alexescg/auctioneer/view/MessageListActivity.kt
@@ -0,0 +1,14 @@
+package github.com.alexescg.auctioneer.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import github.com.alexescg.auctioneer.R
+
+class MessageListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message_list)
+    }
+}
