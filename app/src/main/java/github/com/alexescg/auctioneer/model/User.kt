6fe3b591d5bde3337f642a7f89ee4c0527a7fde2
alex <aeg0204@gmail.com>
2017-05-28 20:57:10 -0600

    Add name field

--- app/src/main/java/github/com/alexescg/auctioneer/model/User.kt
+++ app/src/main/java/github/com/alexescg/auctioneer/model/User.kt
@@ -6,4 +6,7 @@ package github.com.alexescg.auctioneer.model
  * @author alex
  * @since 5/26/17.
  */
-data class User(val email: String, val password: String?, val avatar: String) : Model()
+data class User(val email: String,
+                val password: String?,
+                val avatar: String?,
+                val name: String?) : Model()
